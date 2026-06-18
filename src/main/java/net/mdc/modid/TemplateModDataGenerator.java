6149de57648b31198bb95948c1d011d705dcfd13
overcookedcircuit <manaspatel31122004@gmail.com>
2026-06-18 05:18:56 -0400

    modded item texture added

--- src/main/java/net/mdc/modid/TemplateModDataGenerator.java
+++ src/main/java/net/mdc/modid/TemplateModDataGenerator.java
@@ -2,10 +2,13 @@ package net.mdc.modid;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.mdc.modid.datagen.ModModelProvider;
 
 public class TemplateModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		var pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
 	}
 }
---
+++ src/main/java/net/mdc/modid/datagen/ModModelProvider.java
@@ -0,0 +1,24 @@
+package net.mdc.modid.datagen;
+
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.mdc.modid.item.ModItems;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.model.ModelTemplates;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricPackOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockModelGenerators blockModelGenerators) {
+
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerators itemModelGenerators) {
+        itemModelGenerators.generateFlatItem(ModItems.FLUORITE, ModelTemplates.FLAT_ITEM);
+    }
+}
